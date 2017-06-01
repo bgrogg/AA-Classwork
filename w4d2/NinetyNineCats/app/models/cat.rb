406e03b5293483d064386b95ebc5b52e52e80a38
bgrogg <bgrogg3@gmail.com>
2017-06-01 12:33:00 -0700

    Add 99Cats

---
+++ w4d2/NinetyNineCats/app/models/cat.rb
@@ -0,0 +1,30 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :integer          not null, primary key
+#  birth_date  :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+require 'action_view'
+
+class Cat < ActiveRecord::Base
+  include ActionView::Helpers::DateHelper
+
+  CAT_COLORS = ["black", "white", "orange", "pink", "green"]
+
+  validates :birth_date, :color, :name, :sex, presence: true
+  validates :color, inclusion: CAT_COLORS
+  validates :sex, inclusion: ["M", "F"]
+
+
+  def age
+    time_ago_in_words(birth_date)
+  end
+end
