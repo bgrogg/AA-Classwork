c3a21ae6a1305fb4525357aee5e95ec4c47bf81d
bgrogg <bgrogg3@gmail.com>
2017-11-10 14:08:46 -0800

    add move function

---
+++ w6d1/asteroids/lib/moving_object.js
@@ -0,0 +1,40 @@
+
+
+function MovingObject(options) {
+  this.pos = options.pos;
+  this.vel = options.vel;
+  this.radius = options.radius;
+  this.color = options.color;
+  // this.game = options.game;
+}
+
+MovingObject.prototype.draw = function draw(ctx) {
+  ctx.fillStyle = this.color;
+
+  ctx.beginPath();
+  ctx.arc(
+    this.pos[0], this.pos[1], this.radius, 0, 2 * Math.PI, true
+  );
+  ctx.fill();
+};
+
+const NORMAL_FRAME_TIME_DELTA = 1000 / 60;
+
+MovingObject.prototype.move = function move(timeDelta) {
+  const velocityScale = timeDelta / NORMAL_FRAME_TIME_DELTA,
+    offsetX = this.vel[0] * velocityScale,
+    offsetY = this.vel[1] * velocityScale;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = MovingObject;
