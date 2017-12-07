1d7a2c3568df258b3253576647f9f04ec71d73ea
bgrogg <bgrogg3@gmail.com>
2017-12-06 16:00:24 -0800

    added .gitignore file and import game logic

---
+++ w6d5/minesweeper/components/game.jsx
@@ -0,0 +1,15 @@
+
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    const board = new Minesweeper.Board(9, 10);
+    this.state = { board: board };
+;  }
+
+  render() {
+
+  }
+}
+
+export default Game;
